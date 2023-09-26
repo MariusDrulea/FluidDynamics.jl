f7edf2479981f257256df63be632e8b3ff12a40a
MariusDrulea <marius.drulea@gmail.com>
2023-09-26 18:39:16 +0300

    implement diffusion! and sketch the diffusion_step

--- src/diffusion.jl
+++ src/diffusion.jl
@@ -1,21 +1,41 @@
-module Diffusion
+# module Diffusion
+    using StaticArrays
+
     struct Cell
         density::Float64
+        velocity::SVector{3, Float64}
     end
 
     H, W, D = 8, 8, 8
 
-    zeros(8, 8, 8)
     Matrix3 = Array{Cell, 3} # this is a typedef
     
-    function diffusion!(volume::Matrix3)::Matrix3
+    randCell() = Cell(rand(), SA[rand(), rand(), rand()])
+    randCell()
+
+    volume = [randCell() for _ in 1:H, _ in 1:W, _ in 1:D]
+
+    function diffusion_step(volume_in::Matrix3, volume_out::Matrix3)::Matrix3        
+       kernel = ones(3, 3, 3) # TODO: use gaussians
+    #    TODO: keep same amount of water - problems in boundaries!
+    #    conv3(volume, kernel)
+        return volume_out
+    end
+
+    function diffusion!(volume, max_steps)::Matrix3
+        volume_tmp = similar(volume)
+        volumes = (volume, volume_tmp)
         
+        cnt = 1
+        while cnt <= max_steps
+            diffusion_step(volumes[cnt%2], volumes[(cnt+1)%2])
+            cnt+=1
+        end
+        
+        cnt%2 == 0 && (volume .= volume_tmp)
+        
+        return volume
     end
 
-    function diffusion(volume::Matrix3)::Matrix3
-        volume2 = similar(volume)
-        return diffusion!(volume2)
-    end
-
-    
-end
+    @time diffusion(volume, 10);
+# end
