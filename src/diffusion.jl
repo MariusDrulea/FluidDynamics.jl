f47c95aa8f75412d254fc623d62b8c4baa2c205e
Bogdan Morosanu <bogdan.moro.mail@gmail.com>
2023-10-04 18:03:24 +0200

    Add diffusion v1

--- src/diffusion.jl
+++ src/diffusion.jl
@@ -1,5 +1,51 @@
-# module Diffusion
+module Diffusion
+
+
+#      c
+#      
+# c    c'   c
+#      
+#      c
+#
+# sum(c) = constant (cantitatea de lichid nu creste sau scade)
+#
+# c(t, x) - c(t', x) = sum(flow(c(t, x), c(t, y))
+#
+# k   : 0.25  0.5  0.25
+# c(0): 0     1    2     3
+#         ->
+# flow(c(0, 0), c(0, 1)) = c(0,0) * k(0) - c(0,1) * k(2)
+#
+# sum(flow(c(0, 0), c(0, k))) = 
+#
+# sum(k) == 1
+# 
+# c(t+1, ...) = c + conv(c(t, ...), k)
+
+# conv
+# k
+# 0.33 0.33 0.33
+#
+# data                             t
+# 0    0    1    0                 |
+# 0    0.33 0.33 0.33              V
+# 1/9  2/9  3/9  2/9   1/9      
+# 0    0    1    
+# 
+
+# conv with adding back padding 
+# k
+# 0.33 0.33 0.33
+#
+# data
+# 0    0    1    0
+# 0    0.33 0.66 x
+# 1/9  2/9  6/9  x
+# 0    0    0    x   
+
     using StaticArrays
+    using NNlib
+    using Flux
 
     struct Cell
         density::Float64
@@ -15,14 +61,18 @@
 
     volume = [randCell() for _ in 1:H, _ in 1:W, _ in 1:D]
 
-    function diffusion_step(volume_in::Matrix3, volume_out::Matrix3)::Matrix3        
-       kernel = ones(3, 3, 3) # TODO: use gaussians
-    #    TODO: keep same amount of water - problems in boundaries!
-    #    conv3(volume, kernel)
+    function diffusion_kernel()
+        k = ones(3, 3, 3)
+        return k ./ length(k)
+    end
+
+    function diffusion_step(volume_in, volume_out)
+        k = diffusion_kernel()
+        volume_out = conv(NNlib.pad_repeat(volume_in, (1, 1, 1, 1, 1, 1)), k)
         return volume_out
     end
 
-    function diffusion!(volume, max_steps)::Matrix3
+    function diffusion!(volume, max_steps)
         volume_tmp = similar(volume)
         volumes = (volume, volume_tmp)
         
@@ -37,5 +87,4 @@
         return volume
     end
 
-    @time diffusion(volume, 10);
-# end
+end
